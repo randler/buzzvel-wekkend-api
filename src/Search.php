e1c4e91af26426d8f05c9dcdd458989db789654a
randler.ferraz <randler.ferraz@codificar.com.br>
2021-10-13 08:37:48 -0300

    fix get distance two points

--- src/Search.php
+++ src/Search.php
@@ -82,32 +82,30 @@ class Search
 
     // Calculate distance between two points in latitude and longitude in km's or miles
     public static function distance(
-        $lat1, 
-        $lon1, 
-        $lat2, 
-        $lon2, 
-        $unit = "km"
-    ) 
-    {    
-        $lat1 = floatval($lat1); 
-        $lon1 = floatval($lon1); 
-        $lat2 = floatval($lat2); 
+        $lat1,
+        $lon1,
+        $lat2,
+        $lon2
+    )
+    {
+        $lat1 = floatval($lat1);
+        $lon1 = floatval($lon1);
+        $lat2 = floatval($lat2);
         $lon2 = floatval($lon2);
 
-        $theta = $lon1 - $lon2;
-        $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
-        $dist = acos($dist);
-        $dist = rad2deg($dist);
-        $miles = $dist * 60 * 1.1515;
-        $unit = strtoupper($unit);
-
-        if ($unit == "KM") {
-            return number_format(($miles * 1.609344), 2, '.', '') . " KM";
-        } else if ($unit == "M") {
-            return number_format(($miles * 0.8684), 2) . " M";
-        } else {
-            return number_format($miles, 2);
-        }
+        $pi80 = M_PI / 180;
+        $lat1 *= $pi80;
+        $lon1 *= $pi80;
+        $lat2 *= $pi80;
+        $lon2 *= $pi80;
+        $r = 6372.797; // mean radius of Earth in km
+        $dlat = $lat2 - $lat1;
+        $dlon = $lon2 - $lon1;
+        $a = sin($dlat / 2) * sin($dlat / 2) + cos($lat1) * cos($lat2) * sin($dlon / 2) * sin($dlon / 2);
+        $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
+        $km = $r * $c;
+        
+        return $km;
     }
 
     //make request hotels
